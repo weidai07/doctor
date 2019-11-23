5ed2469844916b1e77fcaca8d96826f3055562eb
Wei Dai <wei@>
2019-11-22 16:46:54 -0800

    show that project is incomplete and will be resubmitted soon

--- src/main.js
+++ src/main.js
@@ -10,8 +10,7 @@ $(document).ready(function() {
 
   let doctor = $("#doctor").val();
   let name = $("#name").val();
-  let name = $("#firstName").val();
-  let name = $("#lastName").val();
+
 
     let request = new XMLHttpRequest();
     const url = `https://cors-anywhere.herokuapp.com/https://api.betterdoctor.com/2016-03-01/doctors?first_name=${name}&last_name=${name}&specialty_uid=${doctor}&location=Portland&fields=${name}&limit=50&user_key=40b1529ebd6af1e603feb3e6b4b724ad`;
@@ -29,18 +28,9 @@ $(document).ready(function() {
     const getElements = function(response) {
       let docInfo = response.doc.length;
 
-      $(".display-name").show();
+      $(".display-name").text();
 
     }
 
   });
 });
-
-
-// App name
-// Epicodus's App
-// Key
-// 40b1529ebd6af1e603feb3e6b4b724ad
-// Add this as a user_key parameter to your API calls to authenticate.
-// API_KEY = 40b1529ebd6af1e603feb3e6b4b724ad
-// ${doctor}
